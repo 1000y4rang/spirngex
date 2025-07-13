4acce40a943c0167bcbe702bca329bc0f01567b7
1000y4rang <1000y4rang@hanmail.net>
2025-07-13 15:42:27 +0900

    최초 커밋

---
+++ src/main/java/com/zerock/springex/sample/SampleDAO.java
@@ -0,0 +1,7 @@
+package com.zerock.springex.sample;
+
+import org.springframework.stereotype.Repository;
+
+/*@Repository*/
+public interface SampleDAO {
+}
---
+++ src/test/java/com/zerock/springex/sample/SampleTests.java
@@ -0,0 +1,24 @@
+package com.zerock.springex.sample;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)  // spring-test를 이용하기 위한 설정. (JUnit5버전)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/root-context.xml")
+public class SampleTests {
+
+    @Autowired
+    private SampleService sampleService;
+
+    @Test
+    public void testService() {
+        log.info("sampleService : {}", sampleService);
+        Assertions.assertNotNull(sampleService);
+    }
+}
